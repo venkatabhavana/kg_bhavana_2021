2ccfbe3023ab10ff49da9491f4b9a858a144cdb3
venkatabhavana <83297148+venkatabhavana@users.noreply.github.com>
2021-05-03 12:08:16 -0400

    Create main.js
    Created main.js file with hard input

---
+++ main.js
@@ -0,0 +1,52 @@
+var x = [1, 2, 3]
+
+var i;
+var output = "";
+
+for (i = 0; i < x.length; i++) {
+  var digit = ("" + x[i]).split("");
+  var j;
+  var number_string = "";
+  for (j = 0; j < digit.length; j++) {
+    var str;
+    switch (digit[j]) {
+      case "0":
+        str = "Zero";
+        break;
+      case "1":
+        str = "One";
+        break;
+      case "2":
+        str = "Two";
+        break;
+      case "3":
+        str = "Three";
+        break;
+      case "4":
+        str = "Four";
+        break;
+      case "5":
+        str = "Five";
+        break;
+      case "6":
+        str = "Six";
+        break;
+      case "7":
+        str = "Seven";
+        break;
+      case "8":
+        str = "Eight";
+        break;
+      case "9":
+        str = "Nine";
+        break;
+      
+    }
+    number_string = number_string + str;
+  }
+  if (x.length == i + 1) {
+    output = output + number_string;
+  } else output = output + number_string + ", ";
+}
+
+console.log(output);
